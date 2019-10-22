55816f8ff1e9eec87bcf86ac1cb9c95c098b31ac
hawaiianpizzaguy <hawaiianpizzaguy@gmail.com>
2019-10-22 01:32:10 -0700

    updated file structure

--- src/main/java/com/hawaiianpizzaguy/nothingcraft/NothingCraft.java
+++ src/main/java/com/hawaiianpizzaguy/nothingcraft/NothingCraft.java
@@ -1,31 +1,13 @@
 package com.hawaiianpizzaguy.nothingcraft;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("nothingcraft")
 public class NothingCraft
 {
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
 
-    public NothingCraft() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    public static final String MOD_ID = "nothingcraft";
 
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-    }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        // some preinit code
-
-    }
 
 }
